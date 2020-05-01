46baeb17f30c0bd91cbb3ce9767ce4986d41f595
Pablo Martínez Figueroa <pablomf@hotmail.es>
2020-05-01 13:42:07 +0200

    [test] Añadidos más tests
    Se han añadido más tests de integración a 'app_test.dart' que comprueban que funciona todo como debería en la app.

--- test_driver/app_test.dart
+++ test_driver/app_test.dart
@@ -17,11 +17,19 @@ void main() {
 
     final emailField = find.byValueKey('email_field');
     final passwordField = find.byValueKey('password_field');
+    final registrationEmailField = find.byValueKey('registration_email_field');
+    final registrationPassField = find.byValueKey('registration_pass_field');
+    final registrationConfirmField =
+        find.byValueKey('registration_confirm_field');
+
+    final registrationRegisterButton =
+        find.byValueKey('registration_register_button');
 
     final loginpage = find.byType('Login');
     final homepage = find.byType('Home');
     final moviedetailspage = find.byType('MovieDetails');
     final movielistpage = find.byType('AllMovieList');
+    final registrationpage = find.byType('Registration');
 
     test('Unsuccessful login', () async {
       await driver.tap(find.byValueKey('login_button'));
@@ -66,7 +74,7 @@ void main() {
       });
     });
 
-    test('Checking all movies and then loggin out', () async {
+    test('Checking all movies', () async {
       await driver.runUnsynchronized(() async {
         await driver.tap(find.pageBack());
         await driver.waitUntilNoTransientCallbacks();
@@ -81,11 +89,62 @@ void main() {
         await driver.waitUntilNoTransientCallbacks();
         await driver.tap(find.byValueKey('movie_list_prev'));
         await driver.waitUntilNoTransientCallbacks();
+      });
+    });
+
+    test('Logout from app', () async {
+      await driver.runUnsynchronized(() async {
         await driver.tap(find.pageBack());
         assert(movielistpage == null);
         await driver.tap(find.byValueKey('popup_logout_button'));
         await driver.tap(find.byValueKey('logout_button'));
         await driver.waitUntilNoTransientCallbacks();
+      });
+    });
+
+    test('Failing to register new user - Passwords do not coincide', () async {
+      await driver.tap(find.byValueKey('register_button'));
+      await driver.runUnsynchronized(() async {
+        await driver.tap(registrationEmailField);
+        await driver.enterText('pablomfemail.com');
+        await driver.tap(registrationPassField);
+        await driver.enterText('12345');
+        await driver.tap(registrationConfirmField);
+        await driver.enterText('123456');
+        await driver.tap(registrationRegisterButton);
+        assert(registrationpage != null);
+        assert(find.text('Passwords must coincide') != null);
+      });
+    });
+
+    test('Failing to register new user - Not a valid email', () async {
+      await driver.runUnsynchronized(() async {
+        await driver.tap(registrationPassField);
+        await driver.enterText('123456');
+        await driver.tap(registrationRegisterButton);
+        await Future.delayed(Duration(seconds: 4));
+        assert(registrationpage != null);
+      });
+    });
+
+    test('Failing to register new user - Already used email', () async {
+      await driver.runUnsynchronized(() async {
+        await driver.tap(registrationEmailField);
+        await driver.enterText('pablomf@email.com');
+        await driver.tap(registrationRegisterButton);
+        await Future.delayed(Duration(seconds: 4));
+        assert(registrationpage != null);
+      });
+    });
+
+    test('Success registering new user', () async {
+      await driver.runUnsynchronized(() async {
+        await driver.tap(registrationEmailField);
+        await driver.enterText('pablofm@email.com');
+        await driver.tap(registrationRegisterButton);
+        await driver.waitUntilNoTransientCallbacks();
+        assert(registrationpage == null);
+        assert(homepage != null);
       });
     });
   });
